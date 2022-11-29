a2e2ed484c855bfc1b4ac32e721d567c346d98c0
Seung-ho Kim <rhanfwlq980@naver.com>
2022-11-29 15:35:15 +0900

    [ADD] floating-button 생성

---
+++ components/FloatingButton.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Link from "next/link";
+
+interface FloatingButtonProps {
+	children: React.ReactNode;
+	href: string;
+}
+
+export default function FloatingButton({children, href}: FloatingButtonProps) {
+	return (
+		<Link href={href} legacyBehavior>
+			<a
+				className={"fixed hover:bg-violet-900 border-0 aspect-square border-transparent transition-colors cursor-pointer bottom-24 right-5 shadow-xl bg-pantone rounded-full w-14 flex items-center justify-center text-white"}>
+				{children}
+			</a>
+		</Link>
+	);
+}
