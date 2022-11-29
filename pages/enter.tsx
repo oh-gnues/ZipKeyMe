ccaba9875e9b0e482bdc6177d46bf25f3de02959
Seung-ho Kim <rhanfwlq980@naver.com>
2022-11-29 14:39:45 +0900

    [FEAT] 비밀번호 입력 창 타입 수정

--- pages/enter.tsx
+++ pages/enter.tsx
@@ -85,7 +85,7 @@ const Enter: NextPage = () => {
           <Input
             register={register("password")}
             name="password"
-            type="current-password"
+            type="password"
             placeholder="비밀번호를 입력해주세요."
             required
             loginInput
