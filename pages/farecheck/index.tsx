13dbb6b32472fb21d52d61fd9a6526acea1b21fd
Kim-Seo-Gwon <tjrnjs123@gmail.com>
2022-12-01 21:43:18 +0900

    [FEAT] 요금조회페이지(farecheck/index.tsx) - 요금조회 백엔드 연동

--- pages/farecheck/index.tsx
+++ pages/farecheck/index.tsx
@@ -1,28 +1,84 @@
-import type {NextPage} from 'next'
+import type { NextPage } from "next";
 import Layout from "@components/Layout";
 import Head from "next/head";
 import FareMontly from "@components/FareMontly";
+import useSWR from "swr";
+import { Fare } from "@prisma/client";
+import useUser from "@libs/client/useUser";
+import fare from "pages/api/fare";
+
+interface FaresResponse {
+  ok: boolean;
+  fares: Fare[];
+}
 
 const Fare: NextPage = () => {
+  const { data } = useSWR<FaresResponse>("/api/fare");
+  const { user } = useUser();
   return (
-    <Layout title={"요금 조회"} hasTabBar canGoBack>
-      <Head><title>요금 조회</title></Head>
+    <Layout
+      title={"요금 조회"}
+      hasTabBar
+      canGoBack
+    >
+      <Head>
+        <title>요금 조회</title>
+      </Head>
       <div className={"mt-5 space-y-10"}>
-        {[11, 10, 9, 8, 7, 6, 5].map((month, index) => (
+        {data?.fares?.map((fare) => (
           <FareMontly
-            month={month}
-            address={"한양대학로 43-1 202호"}
-            year={2022}
-            fare={123456}
-            dueDateYear={2022}
-            dueDateMonth={month}
-            dueDateDay={30}
-            detailID={index}
+            key={fare.fareId}
+            month={+getDateData(fare).month}
+            address={
+              "디비아파트 " +
+              user?.household.aptDong +
+              "동 " +
+              user?.household.aptHo +
+              "호"
+            }
+            year={+getDateData(fare).year}
+            fare={getTotal(fare)}
+            dueDateYear={
+              getDateData(fare).month == 12
+                ? getDateData(fare).year + 1
+                : getDateData(fare).year
+            }
+            dueDateMonth={
+              getDateData(fare).month == 12 ? 1 : getDateData(fare).month + 1
+            }
+            dueDateDay={+getDateData(fare).day}
+            detailID={fare.fareId}
           />
         ))}
       </div>
     </Layout>
   );
+};
+
+function getTotal(fare: Fare) {
+  let total = 0;
+  total += fare.heating;
+  total += fare.water;
+  total += fare.commonElectricity;
+  total += fare.elevatorElectricity;
+  total += fare.householdWaste;
+  total += fare.management;
+  total += fare.representative;
+  total += fare.buildingInsurance;
+  total += fare.expenses;
+  total += fare.disinfection;
+  total += fare.consignmentManagement;
+  total += fare.longTermRepairs;
+
+  return total;
+}
+function getDateData(fare: Fare): { year: number; month: number; day: number } {
+  const splitFareAt = fare.fareAt.toString().split("-");
+  return {
+    year: +splitFareAt[0],
+    month: +splitFareAt[1],
+    day: 1,
+  };
 }
 
 export default Fare;
