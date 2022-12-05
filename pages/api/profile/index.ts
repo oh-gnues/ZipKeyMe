6ecef9557e08a1ac70a4ae80056b2e6221ffb0e0
Kim-Seo-Gwon <tjrnjs123@gmail.com>
2022-12-05 22:55:20 +0900

    [FEAT] 비밀번호 변경API(api/profile/index) 추가 구현

--- pages/api/profile/index.ts
+++ pages/api/profile/index.ts
@@ -8,30 +8,50 @@ async function handler(req: NextApiRequest, res: NextApiResponse) {
     session: { user },
     body: { target, oldThing, newThing },
   } = req;
-  const isMatch = await client.user.count({
-    where: {
-      id: user?.account,
-      email: oldThing,
-    },
-  });
-  const isUsed = await client.user.count({
-    where: {
-      email: newThing,
-    },
-  });
-  if (!isMatch) {
-    return res.json({ ok: false, message: "기존 이메일이 틀립니다." });
-  } else if (isUsed > 0) {
-    return res.json({ ok: false, message: "이미 사용중인 이메일입니다." });
+  if (target === "email") {
+    const isMatch = await client.user.count({
+      where: {
+        id: user?.account,
+        email: oldThing,
+      },
+    });
+    const isUsed = await client.user.count({
+      where: {
+        email: newThing,
+      },
+    });
+    if (!isMatch) {
+      return res.json({ ok: false, message: "기존 이메일이 틀립니다." });
+    } else if (isUsed > 0) {
+      return res.json({ ok: false, message: "이미 사용중인 이메일입니다." });
+    }
+    await client.user.update({
+      where: {
+        id: user?.account,
+      },
+      data: {
+        email: newThing,
+      },
+    });
+  } else if (target === "password") {
+    const isMatch = await client.user.count({
+      where: {
+        id: user?.account,
+        pwd: oldThing,
+      },
+    });
+    if (!isMatch) {
+      return res.json({ ok: false, message: "기존 비밀번호가 틀립니다." });
+    }
+    await client.user.update({
+      where: {
+        id: user?.account,
+      },
+      data: {
+        pwd: newThing,
+      },
+    });
   }
-  await client.user.update({
-    where: {
-      id: user?.account,
-    },
-    data: {
-      email: newThing,
-    },
-  });
   return res.json({ ok: true, message: "변경이 완료되었습니다." });
 }
 
