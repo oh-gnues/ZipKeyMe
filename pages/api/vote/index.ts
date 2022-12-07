38ca8cfe8d20cdf3451894a1ab3a1b47271b594a
Kim-Seo-Gwon <tjrnjs123@gmail.com>
2022-12-08 06:19:14 +0900

    [ADD] 투표목록 페이지 백 구현

--- pages/api/vote/index.ts
+++ pages/api/vote/index.ts
@@ -6,20 +6,15 @@ import { withSession } from "@libs/server/withSession";
 async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method == "POST") {
     const { candidates, title, startAt, finishAt, reChoice } = req.body;
-    let candidatesInput: { name: string }[] = [];
-    candidates.forEach((candi: { name: string }) => {
-      candidatesInput.push(candi);
-    });
-
     const vote = await client.vote.create({
       data: {
         title,
-        startAt: startAt + "T00:00:00.000Z",
-        finishAt: finishAt + "T00:00:00.000Z",
+        startAt: new Date(startAt),
+        finishAt: new Date(finishAt),
         reChoice,
         candidates: {
           createMany: {
-            data: candidatesInput,
+            data: candidates,
           },
         },
       },
@@ -29,6 +24,10 @@ async function handler(req: NextApiRequest, res: NextApiResponse) {
     });
     console.log(vote);
     res.json({ ok: true, vote });
+  }
+  if (req.method == "GET") {
+    const votes = await client.vote.findMany({});
+    res.json({ ok: true, votes });
   }
 }
 
