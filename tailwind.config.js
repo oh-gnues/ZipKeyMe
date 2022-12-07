523ac37da083da107169adf968a66138cd6b341f
Seung-ho Kim <rhanfwlq980@naver.com>
2022-12-08 01:58:20 +0900

    [ADD] pantone 관련 색상 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,11 @@ module.exports = {
       colors: {
         'pantone': '#6667AB',
         'pantone-light': '#A3A5D9',
+        'pantone-lighter': '#C2C2E0',
+        'pantone-lightest': '#E6E6F2',
         'pantone-dark': '#3D3F6F',
+        'pantone-darker': '#2A2B4A',
+        'pantone-darkest': '#1A1A2E',
       },
     },
   },
