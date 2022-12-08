c9875544c7fded539b5bf228d7b85fa4bfc2ee29
Kim-Seo-Gwon <tjrnjs123@gmail.com>
2022-12-09 02:15:14 +0900

    [FEAT & ADD] 게시글API - 게시글API와 공지API로 세분화

--- pages/api/notice/index.ts
+++ pages/api/notice/index.ts
@@ -5,40 +5,29 @@ import { withSession } from "@libs/server/withSession";
 
 async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method == "GET") {
-    const posts = await client.post.findMany({
-      where: { isNotice: false },
-      include: {
-        users: {
-          select: { name: true },
-        },
-        _count: {
-          select: { likes: true, reples: true },
-        },
+    const notices = await client.post.findMany({
+      where: {
+        isNotice: true,
       },
     });
-    return res.json({ ok: true, posts });
+    return res.json({ ok: true, notices });
   }
 
   if (req.method == "POST") {
     const {
-      session: { user },
       body: { title, content },
     } = req;
 
-    const post = await client.post.create({
+    const notice = await client.post.create({
       data: {
         title,
         content,
-        users: {
-          connect: {
-            id: user?.account,
-          },
-        },
+        isNotice: true,
       },
     });
     return res.json({
       ok: true,
-      post,
+      notice,
     });
   }
   if (req.method == "PATCH") {
@@ -51,6 +40,6 @@ export default withSession(
   apiHandler({
     method: ["GET", "POST"],
     handler,
-    isPrivate: true,
+    isPrivate: false,
   })
 );
